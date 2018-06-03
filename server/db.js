9f10ec26bd393d004e76777f6f691a50d624f943
briansai <brianwsai@gmail.com>
2018-06-03 16:09:53 -0700

    included mysql schema with dummy data and the connection

--- server/db.js
+++ server/db.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'ABCdef123!',
-  database: 'airfec',
+  database: 'airfec'
 });
 
 module.exports.connection = connection;
