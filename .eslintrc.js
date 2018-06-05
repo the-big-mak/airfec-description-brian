ad9921c4090d347cc98d92e731f9d7b30cac4f29
briansai <brianwsai@gmail.com>
2018-06-05 16:45:22 -0700

    Installed webpack and updated package.json. Included .eslintrc.js and .babelrc.js into .gitignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,13 @@
 module.exports = {
-  "extends": "airbnb-base"
+  parser: "babel-eslint",
+  plugins: ["react", "import", "jsx-a11y"],
+  env: {
+    "browser": true,
+    "node": true,
+    "es6": true
+  },
+  rules: {
+    "react/prop-types": [2]
+  },
+  extends: "airbnb-base"
 };
